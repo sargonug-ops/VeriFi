5698b023f174bd827a6e5407517c5abf6f15c74b
srushti0720 <wk.csrushti@gmail.com>
2026-07-01 00:34:49 -0800

    added jsonl format

--- data_ingestion/main.py
+++ data_ingestion/main.py
@@ -1,3 +1,6 @@
+"""
+test commit 2
+"""
 # from pdf_reader import extract_pdf_text
 
 
@@ -32,10 +35,47 @@
 
 # if __name__ == "__main__":
 #     main()
+
+"""
+test commit 3
+"""
+# from pdf_reader import extract_pdf_text
+# from chunker import chunk_pages
+
+# PDF_PATH = "source_files/terms-and-conditions.pdf"
+
+
+# def main():
+#     pages = extract_pdf_text(PDF_PATH)
+
+#     chunks = chunk_pages(
+#         pages,
+#         source_document="terms-and-conditions.pdf"
+#     )
+
+#     print(f"Pages: {len(pages)}")
+#     print(f"Chunks: {len(chunks)}")
+
+#     print("\nFIRST CHUNK\n")
+#     print(chunks[0])
+
+#     print("\nLAST CHUNK\n")
+#     print(chunks[-1])
+
+
+# if __name__ == "__main__":
+#     main()   
+
+"""
+test commit 4
+"""
+
 from pdf_reader import extract_pdf_text
 from chunker import chunk_pages
+from jsonl_writer import write_jsonl
 
 PDF_PATH = "source_files/terms-and-conditions.pdf"
+OUTPUT_PATH = "output/chunks.jsonl"
 
 
 def main():
@@ -46,14 +86,11 @@ def main():
         source_document="terms-and-conditions.pdf"
     )
 
+    write_jsonl(chunks, OUTPUT_PATH)
+
     print(f"Pages: {len(pages)}")
     print(f"Chunks: {len(chunks)}")
-
-    print("\nFIRST CHUNK\n")
-    print(chunks[0])
-
-    print("\nLAST CHUNK\n")
-    print(chunks[-1])
+    print(f"Output written to: {OUTPUT_PATH}")
 
 
 if __name__ == "__main__":
